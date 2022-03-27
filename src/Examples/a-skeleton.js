92aa511a39ba5dcf187580bdd3b5a07eba0cf0fb
Raj Rachchh <89799530+rajrachchh14@users.noreply.github.com>
2022-03-27 21:52:24 +0530

    add-skeleton

---
+++ src/Examples/a-skeleton.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const PropsExample = (props: { name: number }) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  );
+};
+export default PropsExample;
