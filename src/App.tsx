ec63eee82d0f946c9657717e09b7fd3b7fa948c3
Raj Rachchh <89799530+rajrachchh14@users.noreply.github.com>
2022-04-02 15:45:13 +0530

    destructure ex

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import PropsExampleOne from "./Examples/PropsExampleOne";
 import InterFaceExampleTwo from "./Examples/InterFaceExampleTwo";
 import OptionalPropsExampleThree from "./Examples/OptionalPropsExampleThree";
+import DestrctureExampleFour from "./Examples/DestrctureExampleFour";
 
 function App() {
   return (
     <div className="App">
-      <h1>Ts- React Examples</h1>
-      <PropsExampleOne user="raj" />
+      <h1>Typescript- React Ex...</h1>
+      <hr />
+      <DestrctureExampleFour user="raj" />
+      <OptionalPropsExampleThree user="raj" />
       <InterFaceExampleTwo user="raj" age={22} />
-      <OptionalPropsExampleThree user="raj rachchh" />
+      <PropsExampleOne user="raj" />
     </div>
   );
 }
---
+++ src/Examples/DestrctureExampleFour.tsx
@@ -0,0 +1,15 @@
+type ProfileInterface = {
+  user: string;
+  age?: number;
+};
+const DestrctureExampleFour = ({ user, age }: ProfileInterface) => {
+  return (
+    <div>
+      <h3> DestrctureExample </h3>
+      <div>{user}</div>
+      <div>{age || "300"}</div>
+      <hr />
+    </div>
+  );
+};
+export default DestrctureExampleFour;
