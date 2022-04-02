b5aa0740e8eb6353dcc62ab27a5d3d76b1a34e6a
Raj Rachchh <89799530+rajrachchh14@users.noreply.github.com>
2022-04-02 20:33:08 +0530

    ChildrenProps

--- src/Examples/ChildrenPropsEx8.tsx
+++ src/Examples/ChildrenPropsEx8.tsx
@@ -1,19 +1,27 @@
 // ChildrenPropsEx 3
 // React.FC ...
-//This Way Writing Of It Will Default Access Children Props (Not Need To Delacre Children as a type)
+//This Way Writing Of It Will Default Access Children Props
+// (Not Need To Delacre Children as a type)
 
 type MyInterface = {
   user: string;
   salarey: number;
 };
 
-const ChildrenPropsEx8: React.FC<MyInterface> = (props) => {
+// const ChildrenPropsEx8: React.FC<MyInterface> = (props) => {
+
+const ChildrenPropsEx8: React.FC<MyInterface> = ({
+  user,
+  salarey,
+  children
+}) => {
   return (
     <div>
       <h3>ChildrenProps Example 3</h3>
-      <div>Name {props.user}</div>
-      <div> salarey {props.salarey} </div>
-      <div> {props.children}</div>
+
+      <div>Name {user}</div>
+      <div> salarey {salarey} </div>
+      <div> {children}</div>
       <hr />
     </div>
   );
